778400c4e3dd4d6e350c726b929e765a0fd59cf3
Ermias Tsegu <yetabalij@gmail.com>
2025-01-01 11:57:44 +0300

    db connection setup completed.

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+
+dotenv.config(); // Load environment variables
 
 const app = express();
 
 // Middleware
 app.use(express.json());
+
+// Connect to Database
+connectDB();
 
 // Test Route
 app.get('/', (req, res) => {
@@ -11,7 +18,7 @@ app.get('/', (req, res) => {
 });
 
 // Start Server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
